db60b862c70a324e69aadfddec6bcf72ebe205ac
AidenZOFCL <aidenzybell@gmail.com>
2020-09-05 19:04:58 -0400

    Yep, I forgot to Use Git so this is all one Commit :)

---
+++ app/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : '/profile/74Dqk16FinTxvLvNNmCVz2DLj22YLpEKB74UCPuc.jpeg';
+        return '/storage/' . $imagePath;
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
